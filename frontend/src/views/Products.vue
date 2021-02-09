0be56833f63b24822267976c493d88644927dd12
Hariz Hasecic <hariz.hasecic@gmail.com>
2021-02-09 21:04:29 +0100

    products

--- frontend/src/views/Products.vue
+++ frontend/src/views/Products.vue
@@ -4,6 +4,34 @@
 
 		<h1>This is an Products page</h1>
 
+		<button @click="tryme">KLICK ME</button>
+
+		<Overlay :show="showMe" v-on:close="showMe = false">
+			<div class="modalProduct">
+				<div class="modalPic">
+					<img
+						class="modalImage"
+						src="../../../assets/skateboard-generic.png"
+					/>
+				</div>
+
+				<div class="modalInfo">
+					<div class="modalTitle">
+						<span>Tricky</span>
+					</div>
+					<div class="modalSubtitle">
+						<span>Unisex</span>
+					</div>
+					<div class="modalTextInfo">
+						<p>Jag gillar apor som hoppar</p>
+					</div>
+					<div class="modalCash">
+						<span>999 sek</span>
+					</div>
+				</div>
+			</div>
+		</Overlay>
+
 		<div class="container">
 			<div class="card">
 				<div class="cardHeader">
@@ -41,6 +69,30 @@
 		</div>
 	</div>
 </template>
+
+<script>
+import Overlay from '@/components/Overlay'
+export default {
+	methods: {
+		tryme() {
+			this.showMe = !this.showMe
+		},
+	},
+	data() {
+		return {
+			showMe: true,
+		}
+	},
+	computed: {
+		showModal() {
+			return this.showMe
+		},
+	},
+	components: {
+		Overlay,
+	},
+}
+</script>
 
 <style scoped lang="scss">
 @import '@/styles/template.scss';
@@ -96,8 +148,8 @@
 	margin-left: 1rem;
 	border-radius: 10rem;
 	background-color: black;
-  // padding: 0.5rem;
-  padding: 0.3rem 0.8rem;
+	// padding: 0.5rem;
+	padding: 0.3rem 0.8rem;
 	color: $primary-color;
 
 	display: flex;
@@ -105,8 +157,49 @@
 }
 
 .priceSek {
-  position: relative;
-  margin-left:0.2rem;
+	position: relative;
+	margin-left: 0.2rem;
 	font-size: 0.65rem;
+}
+
+.modalProduct {
+	display: flex;
+}
+
+.modalPic {
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-end;
+	max-width: 10rem;
+	background-color: red;
+}
+
+.modalInfo {
+	display: flex;
+	flex-direction: column;
+	max-width: 10rem;
+	background-color: green;
+	padding-left: 1rem;
+	padding-right: 1rem;
+	padding-top: 1rem;
+}
+.modalImage {
+	width: 100%;
+}
+.modalTitle {
+	font-size: 2.5rem;
+	text-align: left;
+}
+.modalSubtitle {
+	font-size: 1.3rem;
+	text-align: left;
+}
+.modalTextInfo {
+	font-size: 1rem;
+	text-align: left;
+}
+.modalCash {
+	font-size: 2rem;
+	text-align: left;
 }
 </style>
