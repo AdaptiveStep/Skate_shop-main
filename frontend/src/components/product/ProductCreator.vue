07aa24e876a6293ae14358c4e003ff5044ae1851
Hariz Hasecic <hariz.hasecic@gmail.com>
2021-02-19 22:25:33 +0100

    refactor product creator

--- frontend/src/components/product/ProductCreator.vue
+++ frontend/src/components/product/ProductCreator.vue
@@ -71,9 +71,127 @@
 </template>
 
 <script>
-export default {}
+import TitledContainer from '@/components/TitledContainer'
+import { mapMutations, mapGetters, mapActions, mapState } from 'vuex'
+
+export default {
+	data() {
+		return {
+			newProduct: {
+				title: '',
+				price: '',
+				shortDesc: '',
+				category: 'clothes',
+				longDesc: '',
+				imgFile: 'hoodie-ash.png',
+				serial: '00000000000',
+			},
+			selectedCategory: 'clothes',
+			categories: [
+				{ text: 'Clothes', value: 'clothes' },
+				{ text: 'Board', value: 'board' },
+				{ text: 'Wheels', value: 'wheels' },
+			],
+		}
+	},
+	methods: {
+		setNewProductPath(path) {
+			this.newProduct.imgFile = path
+		},
+		NewProductSubmit(product) {
+			this.createProduct(product)
+			this.newProduct = {
+				title: '',
+				price: 0,
+				shortDesc: '',
+				category: '',
+				longDesc: '',
+				imgFile: 'hoodie-ash.png',
+				serial: '00000000000',
+			}
+		},
+	},
+	computed: {
+		...mapGetters(['allFiles']),
+	},
+	components: {
+		TitledContainer,
+	},
+}
 </script>
 
 <style scoped lang="scss">
+@import '@/styles/template.scss';
 
+.rows {
+	display: flex;
+
+	flex-direction: column;
+	padding-top: 2rem;
+}
+.allFields {
+	display: flex;
+	flex-wrap: wrap;
+	gap: 1rem;
+	justify-content: space-evenly;
+
+	.dataInputFields {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+}
+
+textarea {
+	resize: none;
+	box-sizing: border-box;
+	margin: 19rem;
+	display: block;
+}
+
+.dataInputDescription {
+	display: flex;
+	flex-direction: column;
+}
+
+.adminSubmit {
+	display: flex;
+	justify-content: flex-end;
+}
+
+.productImage {
+	border-radius: 10%;
+	width: 3rem;
+	height: 3rem;
+	background-color: $shadow-color;
+	transition: background-color 0.5s ease;
+	&:hover {
+		background-color: $primary-color;
+	}
+	&:focus {
+		background-color: $orange-color;
+	}
+}
+
+.dataInputs {
+	display: flex;
+	background-color: grey;
+	gap: 1rem;
+	padding: 1rem;
+	justify-content: space-evenly;
+	flex-wrap: wrap;
+	.dataInputImages {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		gap: 1rem;
+		flex-wrap: wrap;
+		max-width: 15rem;
+
+		.selectNewProductImage {
+			width: 3rem;
+			height: 3rem;
+		}
+	}
+}
 </style>
